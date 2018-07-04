32bc928a83f64902af1c9c633e20da2a22c819e1
Paweł Kuklicz <p.kuklicz@gmail.com>
2018-07-04 21:25:17 +0200

    add base record to database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,4 +21,24 @@ INSERT INTO book_authors (book_id, authors_id) VALUES (3,1);
 INSERT INTO book_authors (book_id, authors_id) VALUES (4,4);
 INSERT INTO book_authors (book_id, authors_id) VALUES (5,2);
 INSERT INTO book_authors (book_id, authors_id) VALUES (5,4);
-INSERT INTO book_authors (book_id, authors_id) VALUES (5,5);
+INSERT INTO book_authors (book_id, authors_id) VALUES (5,5);YYYY-MM-DD HH:MI:SS
+
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (1,'test1','2015-04-06T12:30:15','NEW');
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (2,'test2','2017-07-23T23:20:58','DONE');
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (3,'test3','2018-07-04T15:15:01','CANCEL');
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (4,'test4','2018-01-15T18:26:37','IN_REALIZATION');
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (5,'test5','2017-07-24T03:39:41','WAITING');
+INSERT INTO shop_order (id, comment, crete_time, order_status) VALUES (6,'test6','2017-07-12T08:16:17','NEW');
+
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (1,1);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (1,2);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (1,3);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (1,4);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (1,5);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (2,1);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (2,2);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (6,3);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (3,4);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (4,3);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (5,1);
+INSERT INTO shop_order_books (shop_order_id, books_id) VALUES (5,5);
